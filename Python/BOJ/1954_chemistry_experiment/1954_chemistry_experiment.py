0ebc1f27d76310b81ddb5352eac221d2a0cd76b9
muffin <muffin3864@naver.com>
2023-11-01 22:55:06 +0900

    231101 add_sol

--- Python/BOJ/1954_chemistry_experiment/1954_chemistry_experiment.py
+++ Python/BOJ/1954_chemistry_experiment/1954_chemistry_experiment.py
@@ -49,3 +49,57 @@ result = experiment(reagent_a, reagent_b)
 
 print(result)
 
+
+
+# sol
+
+
+# arr = []
+# ans = 0
+
+# N = int(input())
+
+# for _ in range(N):
+#     arr.append(tuple(map(int, input().split())))
+# M = int(input())
+
+# # N = 1인 경우 따로 처리
+# if N == 1:
+#     ans = arr[0][0]*M + arr[0][1]
+
+# if ans == 0:
+#     # 제일 큰 애부터 숫자 넣을거라서 a 내림차순으로 정렬
+#     arr.sort(reverse=True)
+#     # print(arr)
+
+#     # 제일 큰애한테 쓸 약품
+#     for i in range(1, M-N+1):
+#         # 사용한 약품 양
+#         acc = i
+#         # print('i', i)
+#         maxi = arr[0][0]*i + arr[0][1]
+#         for j in range(1, N):
+#             if (maxi-arr[j][1]) % arr[j][0]:
+#                 # 자연수 아니면 다음조사로 넘어가
+#                 # 전부 continue 였다가 아니어서 바꿈!
+#                 break
+#             tmp = (maxi-arr[j][1]) // arr[j][0]
+#             # 자연수가 아닌경우 넘어가
+#             if tmp <= 0:
+#                 break
+#             # 자연수일 경우, 약물 사용 누적합 구하기
+#             acc += tmp
+#             # print('j, x, maxi', j, tmp, maxi)
+#             # 용량 초과면 돌아가
+#             if acc > M:
+#                 break
+#             # 마지막 약품 조사하고, 누적합이 약물양이라면 성공
+#             if j == N-1 and acc==M:
+#                 ans = maxi
+#                 break
+#         # 답을 구했다면 조사 더이상 하지마!
+#         if ans != 0:
+#             break
+# print(ans)
+
+
